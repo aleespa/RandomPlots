7bbecbc311d7a4b335c908a34683090953cfb372
Alejandro López <aleespazx@gmail.com>
2020-01-22 22:06:45 -0600

    up

---
+++ 0120/20012020.py
@@ -0,0 +1,20 @@
+import matplotlib.pylab as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from math import cos, sin,log,tan,gamma,pi,exp,sqrt
+p = plt.figure(figsize=(14,14),facecolor='black',dpi=400)
+p = plt.axis('off')
+p = plt.xlim(-1.4,1.4)
+colors=['#c0d8c6','#84d290','#80d091','#427c5f','#46ab9b']*100
+for z in range(2,100):
+    plt.plot([cos(z+y) for y in np.linspace(0,2*pi,400)],
+             [y*z for y in np.linspace(0,2*pi,400)],
+             lw=2.5,color=colors[z],alpha=0.8)
+    plt.plot([cos(z+0.2*y) for y in np.linspace(0,2*pi,400)],
+             [-y*z for y in np.linspace(0,2*pi,400)],
+             lw=2.5,color=colors[z],alpha=0.8)
+    plt.plot([cos(z+0.1*y) for y in np.linspace(0,2*pi,400)],
+             [y*z+400 for y in np.linspace(0,2*pi,400)],
+             lw=2.5,color=colors[z],alpha=0.9)
+plt.savefig(f'C:/Users/Alejandro/Pictures/RandomPlots/20012020.png',facecolor='black')
---
+++ 0120/21012020.py
@@ -0,0 +1,14 @@
+import matplotlib.pylab as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from math import cos, sin,log,tan,gamma,pi,exp,sqrt
+p = plt.figure(figsize=(14,14),facecolor='black',dpi=400)
+p = plt.axis('off')
+p = plt.xlim(-1,-0.5)
+p = plt.ylim(-1,-0.5)
+for _ in range(14):
+    X,Y= [np.random.uniform(-1,1) for y in np.linspace(0,1,3000)], [np.random.uniform(-1,1) for y in np.linspace(0,1,3000)]
+    #plt.fill_between(X,Y, color='black', alpha=0.4)
+    plt.plot(X,Y,alpha=0.7,lw=0.4)
+plt.savefig(f'C:/Users/Alejandro/Pictures/RandomPlots/21012020.png',facecolor='black')
