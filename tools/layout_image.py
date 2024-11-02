d78af66ef5d02bf1b426d775b179e4494a12bc82
Alejandro <aleespazx@gmail.com>
2024-11-02 15:09:39 +0000

    layout correction

--- tools/layout_image.py
+++ tools/layout_image.py
@@ -1,6 +1,8 @@
 import gc
-import sys
+import random
+import string
 
+import toml
 from loguru import logger
 from matplotlib import pyplot as plt
 
@@ -8,13 +10,17 @@ from tools.technology import create_directory, clear_folder
 
 
 def generate():
-    filename = sys.argv[1]
+    config = toml.load('config.toml')
+    filename = config['file_to_run']
     create_directory(f"outputs/{filename}")
     clear_folder(f"outputs/{filename}")
 
     fig, _ = plt.subplots(figsize=(12, 12), dpi=100)
     ax = fig.add_axes([0, 0, 1, 1], facecolor='#f4f0e7')
     ...
-    fig.savefig(f'outputs/{filename}/figure.png', facecolor='k')
+    name = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+    logger.info(f"{name}.png Saved")
+    fig.savefig(f'outputs/{filename}/{name}.png', facecolor='k')
     logger.info(f"Finished")
+    plt.close()
     gc.collect()
