6c770a8aee0e41aa231ac3c3960fcc8c9dd65e38
Alejandro <aleespazx@gmail.com>
2024-11-03 12:38:22 +0000

    small tweak to layout_image.py

--- tools/layout_image.py
+++ tools/layout_image.py
@@ -16,7 +16,7 @@ def generate():
     clear_folder(f"outputs/{filename}")
 
     fig, _ = plt.subplots(figsize=(12, 12), dpi=100)
-    ax = fig.add_axes([0, 0, 1, 1], facecolor='#f4f0e7')
+    ax = fig.add_axes((0, 0, 1, 1), facecolor='#f4f0e7')
     ...
     name = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
     logger.info(f"{name}.png Saved")
