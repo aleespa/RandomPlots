2ca7e171c592c883285797af59e99d3e8319f9e2
Alejandro <aleespazx@gmail.com>
2026-03-15 10:45:29 +0000

    Unify image saving logic in `ImageProcessingSettings` and add black

--- common/image_processing.py
+++ common/image_processing.py
@@ -8,29 +8,30 @@ import numpy as np
 from matplotlib import pyplot as plt
 from numpy.random import Generator
 
-OUTPUT_PATH = Path('outputs')
+OUTPUT_PATH = Path(__file__).parent.parent / 'outputs'
 
 
 class ImageProcessingSettings:
     filename: str
+    output_path: Path = OUTPUT_PATH
     rng: Generator
 
     def __init__(self, seed=0, filename=None):
         self.filename = filename or datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f')
         self.rng = np.random.default_rng(seed=seed)
-        create_directory(OUTPUT_PATH / self.filename)
-        clear_folder(OUTPUT_PATH / self.filename)
+        create_directory(self.output_path / self.filename)
+        clear_folder(self.output_path / self.filename)
 
-    def save_to_png(self, face_color='#000000'):
-        plt.savefig(
-            OUTPUT_PATH / self.filename / 'figure.png',
-            facecolor=face_color
+    def save_to_png(self, fig:plt.Figure, bg_color='#000000'):
+        time_string = datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f')
+        fig.savefig(
+            self.output_path / self.filename / f'{time_string}.png', facecolor=bg_color
         )
 
     def save_frame(self, face_color='#000000'):
         time_string = datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f')
         plt.savefig(
-            OUTPUT_PATH / self.filename / f'{time_string}.png',
+            self.output_path / self.filename / f'{time_string}.png',
             facecolor=face_color,
         )
         plt.close()
@@ -45,23 +46,28 @@ class ImageProcessingSettings:
         - video_name: Name of the output video file (including .mp4 extension).
         - fps: Frames per second for the output video.
         """
-        images = [img for img in os.listdir(f'outputs/{self.filename}')
-                  if img.endswith(".PNG") or img.endswith(".png")]
+        images = [
+            img
+            for img in os.listdir(OUTPUT_PATH / self.filename)
+            if img.endswith(".PNG") or img.endswith(".png")
+        ]
         images.sort()  # Make sure the images are in the correct order
 
         # Get the size of the images
-        frame = cv2.imread(os.path.join(f'outputs/{self.filename}', images[0]))
+        frame = cv2.imread(str(OUTPUT_PATH / self.filename / images[0]))
         h, w, layers = frame.shape
         size = (w, h)
 
         # Create a video writer object
-        out = cv2.VideoWriter((OUTPUT_PATH
-                              / (self.filename + self.filename + '.mp4')).__str__(),
-                              cv2.VideoWriter_fourcc(*'avc1'),
-                              fps, size)
+        out = cv2.VideoWriter(
+            (OUTPUT_PATH / (self.filename + self.filename + '.mp4')).__str__(),
+            cv2.VideoWriter_fourcc(*'avc1'),
+            fps,
+            size,
+        )
 
         for image in images:
-            img_path = os.path.join(OUTPUT_PATH / self.filename, image)
+            img_path = str(self.output_path / self.filename / image)
             img = cv2.imread(img_path)
             out.write(img)
 
--- lab/2026/0314.py
+++ lab/2026/0314.py
@@ -1,5 +1,4 @@
 import gc
-from datetime import datetime
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -8,7 +7,6 @@ from common.image_processing import ImageProcessingSettings
 
 
 def generate(settings=ImageProcessingSettings(1)):
-    filename = settings.filename
     bg_color = "k"
     fig, ax = plt.subplots(figsize=(12, 12), dpi=200, tight_layout=True)
     fig.patch.set_facecolor(bg_color)
@@ -20,7 +18,9 @@ def generate(settings=ImageProcessingSettings(1)):
         y = np.sin(t * np.sin(t) - np.cos(t))
         plt.plot(x,y,lw=2, color='white')
 
-    time_string = datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f')
-    fig.savefig(f'outputs/{filename}/{time_string}.png', facecolor=bg_color)
+    settings.save_to_png(fig, bg_color)
     plt.close()
     gc.collect()
+
+if __name__ == '__main__':
+    generate()
